9cce397ee473917809a14afff262119a7f6c7b24
mcarrasqub <160037152+mcarrasqub@users.noreply.github.com>
2024-05-20 10:17:41 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -12,16 +12,33 @@ public class Main {
             switch (opcion) {
                 case 'A':
                     coleccion.leerDatos();
-                    break;
+                    System.out.println("Los datos de su archivo se han leído y almacenado correctamente");
+                
                 case 'B':
-                    
-                    break;
+                    System.out.println("ingrese el nombre de la persona a agregar");
+                    String nombre = scanner.next();
+                    int[] respuestas = new int[5];
+                    System.out.println("Ingrese las respuestas a las preguntas (valores entre 0 y 10):");
+                    System.out.println("1. ¿Qué tanto te gusta escuchar música?");
+                    System.out.println("2. ¿Qué tanto te gusta estudiar?");
+                    System.out.println("3. ¿Qué tanto te gusta dibujar?");
+                    System.out.println("4. ¿Qué tanto te gusta ejercitarte?");
+                    System.out.println("5. ¿Qué tanto te gusta dormir?");
+                    for(int i=0; i<respuestas.length; i++){
+                        System.out.println("respuesta " + (i+1) + ": ");
+                        respuestas[i] = scanner.nextInt();
+                    }
+                    Persona nuevaPersona = new Persona(nombre, respuestas);
+                    coleccion.agregarPersona(nuevaPersona);
+                    System.out.println("Persona agregada correctamente.");
+                
                 case 'C':
                     coleccion.escribirDatos();
-                    break;
+                    System.out.println("Datos escritos en el archivo.");
+                    
                 case 'D':
                     
-                    break;
+                    
                 case 'E':
                     System.out.println("Fin del programa.");
                     break;
@@ -35,8 +52,8 @@ public class Main {
     static void mostrarMenu() {
         System.out.println("\nMenú:");
         System.out.println("A. Leer el archivo con los datos");
-        System.out.println("B. Recibir nuevas personas con sus respuestas");
-        System.out.println("C. Escribir el conjunto de datos al archivo");
+        System.out.println("B. Recibir nueva persona con sus respuestas");
+        System.out.println("C. Escribir el conjunto de datos en el archivo");
         System.out.println("D. Encontrar la persona más afín");
         System.out.println("E. Terminar");
         System.out.println();
