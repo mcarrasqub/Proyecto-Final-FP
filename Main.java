68524754278f0ff8bc99d5750be617348a6cd69c
Anderseon <116851168+Anderseon@users.noreply.github.com>
2024-05-20 11:12:16 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -20,8 +20,31 @@ public class Main {
                     coleccion.escribirDatos();
                     break;
                 case 'D':
-                    
-                    break;
+                     if((coleccion.getPersonas().size() < 2)){
+                        System.out.println("No hay suficientes personas para hacer los matches");
+                        continue;
+                    }
+                    coleccionMatches coleccionMatch = new coleccionMatches();
+                    System.out.println("Elige la persona con la que se harán los matches (exactamente): ");
+                    String elegido = scanner.next();
+                    int posElegido = coleccion.buscarPersona(elegido);
+                    if(posElegido == -1){
+                        System.out.println("Esta persona no está");
+                        continue;
+                    }
+                    for(int i = 0; i < (coleccion.getPersonas()).size(); i++){
+                        Persona personai = (coleccion.getPersonas()).get(i);
+                        Persona personaElegida = (coleccion.getPersonas()).get(posElegido);
+                        if(posElegido != i){
+                            double afinidad = coleccion.match(personaElegida, personai);
+                            String nombres = personaElegida.getNombre() + " - " + personai.getNombre();
+                            Matches temp =  new Matches(afinidad, nombres);
+                            coleccionMatch.agregarMatches(temp);
+                        }
+                    }
+                    coleccionMatch.selectionSortArraylistDescendente();
+                    coleccionMatch.imprimirMatches();
+                    continue;
                 case 'E':
                     System.out.println("Fin del programa.");
                     break;
